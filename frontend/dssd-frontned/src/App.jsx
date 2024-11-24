188421834bdb8ce24d56a9eb8ac6dc35d038f80c
Facundo Alcorta <facualcorta2017@gmail.com>
2024-11-24 19:31:59 -0300

    cambiando rutas

--- frontend/dssd-frontned/src/App.jsx
+++ frontend/dssd-frontned/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Colectas from './Colectas'
-import ColectaForm from './ColectaForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Colectas from './Colectas';
+import ColectaForm from './ColectaForm';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import 'bootstrap/dist/css/bootstrap.min.css';                     //bootstrap
 
 function App() {
-
   return (
-    <div className="App">
-      <ColectaForm />
-      <Colectas />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/formularioColecta" element={
+            <>
+              <ColectaForm />
+              <Colectas />
+            </>
+          } />
+          <Route path="/formularioEntrega" element={
+            <h1>Fomulario Entrega</h1>
+          } />
+          <Route path="/" element={<h1>Bienvenido a la página principal</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
